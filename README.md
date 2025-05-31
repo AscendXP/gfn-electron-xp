0425a59d0192527a3b26457477b52821c80ef43f
AscendXP <jahym-bruno@live.com>
2025-05-31 04:53:26 -0400

    Clean start

---
+++ README.md
@@ -0,0 +1,63 @@
+# Geforce NOW for Linux
+Go to the [latest release](https://github.com/AscendXP/gfn-electron-xp/releases/latest).
+![Geforce-now](https://github.com/user-attachments/assets/5968ad3b-fabc-40cc-ab86-1f39eda7a8fa)
+
+## 🆕 Latest Changes
+🎹 Added support for switching between up to 5 user accounts using Ctrl+Shift+Alt+[1–5]
+
+✅ Hardware Acceleration enabled by default  
+🛠️ Fixed: Discord RPC, ESC key behavior, and Google account login  
+🛎️ Notifications when the game is ready  
+🧪 Experimental: arm64 & Windows .exe builds (untested) 
+♻️ Auto-builds with latest Electron + Auto-update support (in builds labeled GeForce_NOW_Auto-Update)
+
+## About
+Unofficial client for Nvidia's GeForce NOW game streaming service, providing a native Linux desktop experience and some additional features such as Discord rich presence.
+
+## Disclaimer
+
+This project and its contributors are not affiliated with Nvidia, nor its GeForce NOW product. This repository does not contain any Nvidia / GeForce NOW software. It is simply an Electron wrapper that loads the official GFN web application page, just as it would in a regular web browser.
+
+
+## Manual Installation
+
+ - Go to the [latest release](https://github.com/AscendXP/gfn-electron-xp/releases/latest).
+ - Download the specific file that best fits your distro.
+
+
+# Building from source
+
+## Requirements
+
+You will need to install [npm](https://www.npmjs.com/), the Node.js package manager. On most distributions, the package is simply called `npm`.
+
+## Cloning the source code
+
+Once you have npm, clone the wrapper to a convenient location:
+
+```bash
+git clone https://github.com/AscendXP/gfn-electron-xp.git
+```
+
+## Building
+
+```bash
+npm install
+npm start
+```
+
+On subsequent runs, `npm start` will be all that's required.
+
+## Updating the source code
+
+Simply pull the latest version of master and install any changed dependencies:
+
+```bash
+git checkout master
+git pull
+npm install
+```
+
+# Links
+ - [GeForce NOW](https://nvidia.com/en-eu/geforce-now)
+ - [Troubleshooting](https://github.com/hmlendea/gfn-electron/wiki/Troubleshooting)
---
+++ scripts/main.js
@@ -0,0 +1,166 @@
+const { app, BrowserWindow, session, Notification, globalShortcut } = require('electron');
+const fs = require('fs');
+const path = require('path');
+const { DiscordRPC } = require('./rpc.js');
+// const { switchFullscreenState } = require('./windowManager.js'); // let electron handle the fullscreen and not manually setting it
+const homePage = 'https://play.geforcenow.com/mall/';
+const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36';
+console.log('Using user agent: ' + userAgent);
+console.log('Process arguments: ' + process.argv);
+app.commandLine.appendSwitch('log-level', '3');
+app.commandLine.appendSwitch('ignore-gpu-blocklist');
+app.commandLine.appendSwitch('enable-accelerated-video');
+app.commandLine.appendSwitch('enable-zero-copy');
+app.commandLine.appendSwitch('enable-gpu-rasterization');
+app.commandLine.appendSwitch('enable-oop-rasterization');
+app.commandLine.appendSwitch('ozone-platform-hint', 'auto');
+app.commandLine.appendSwitch('enable-wayland-ime');
+app.commandLine.appendSwitch('wayland-text-input-version', '2');
+app.commandLine.appendSwitch('enable-features', 'VaapiVideoDecoder,AcceleratedVideoEncoder,VaapiIgnoreDriverChecks,VaapiOnNvidiaGPUs,VaapiVideoDecodeLinuxGL,AcceleratedVideoDecodeLinuxGL,UseOzonePlatform,TouchpadOverscrollHistoryNavigation');
+process.on('uncaughtException', (err) => {
+  console.error('Ignoring uncaught exception:', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Ignoring unhandled promise rejection:', reason);
+});
+
+const configPath = path.join(app.getPath('userData'), 'config.json');
+const config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(configPath, 'utf-8')) : { crashCount: 0 };
+
+let discordIsRunning = false;
+let notified = false;
+
+function isDiscordRunning() {
+  return new Promise(resolve => {
+    resolve(true);
+  });
+}
+
+const userSessions = {
+  Main: null,
+  User2: null,
+  User3: null,
+  User4: null,
+  User5: null,
+};
+
+function createWindowForUser(userId) {
+  for (const [id, win] of Object.entries(userSessions)) {
+    if (id !== userId && win && !win.isDestroyed()) {
+      win.close();
+      userSessions[id] = null;
+    }
+  }
+
+  const partitionName = `persist:${userId}`;
+  const partition = session.fromPartition(partitionName);
+
+  if (!partition._websocketHooked) {
+    partition.webRequest.onBeforeRequest(
+      { urls: ["wss://*/*"] },  // Thanks AstralVixen for this part of the code.
+      async (details, callback) => {
+        const url = details.url;
+        const isNvidiaRequest = url.includes("nvidiagrid") && url.includes("/sign_in") && url.includes("peer_id");
+
+        if (isNvidiaRequest) {
+          const window = BrowserWindow.getAllWindows()[0];
+          if (window && !notified) {
+            const title = await window.webContents.getTitle();
+            console.log(`[GeForce NOW] Current title: "${title}"`);
+
+            new Notification({
+              title: "GeForce NOW",
+              body: `${title.replace(' on GeForce NOW', '')} is ready to play`,
+              icon: path.join(__dirname, "icon.png")
+            }).show();
+
+            notified = true;
+          }
+        }
+
+        callback({ cancel: false });
+      }
+    );
+
+    partition._websocketHooked = true;
+  }
+
+  if (!userSessions[userId]) {
+    userSessions[userId] = new BrowserWindow({
+      fullscreenable: true,
+      show: false,
+      icon: path.join(__dirname, "icon.png"),
+     webPreferences: {
+    session: session.fromPartition(partitionName),
+    preload: path.join(__dirname, 'preload.js'),
+    contextIsolation: false,
+    userAgent: userAgent,},
+    });
+
+    userSessions[userId].loadURL(homePage);
+
+    userSessions[userId].once('ready-to-show', () => {
+      userSessions[userId].show();
+      new Notification({
+        title: "Account Active",
+        body: `Now using ${userId}`,
+        icon: path.join(__dirname, "icon.png")
+      }).show();
+    });
+
+    userSessions[userId].on('page-title-updated', async (e, title) => {
+      if (title.includes(" on GeForce NOW")) {
+        userSessions[userId].maximize();
+        notified = false;
+      }
+    });
+  } else {
+    userSessions[userId].focus();
+  }
+}
+
+app.whenReady().then(async () => {
+  discordIsRunning = await isDiscordRunning();
+
+  createWindowForUser('Main');
+
+  if (discordIsRunning) {
+    DiscordRPC('GeForce NOW');
+  }
+
+  globalShortcut.register('Control+Shift+Alt+1', () => createWindowForUser('Main'));
+  globalShortcut.register('Control+Shift+Alt+2', () => createWindowForUser('User2'));
+  globalShortcut.register('Control+Shift+Alt+3', () => createWindowForUser('User3'));
+  globalShortcut.register('Control+Shift+Alt+4', () => createWindowForUser('User4'));
+  globalShortcut.register('Control+Shift+Alt+5', () => createWindowForUser('User5'));
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindowForUser('Main');
+    }
+  });
+});
+
+app.on('browser-window-created', async function (e, window) {
+  window.setBackgroundColor('#1A1D1F');
+  window.setMenu(null);
+  window.webContents.setUserAgent(userAgent);
+
+  window.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    BrowserWindow.getAllWindows()[0]?.loadURL(url);
+  });
+
+  if (discordIsRunning) {
+    window.on('page-title-updated', async function (e, title) {
+      DiscordRPC(title);
+    });
+  }
+});
+
+app.on('window-all-closed', async function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
